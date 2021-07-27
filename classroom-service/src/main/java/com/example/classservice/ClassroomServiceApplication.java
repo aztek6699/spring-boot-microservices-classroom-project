16339175930715ec3238d832ec2fe7e65d0de941
zain.haque <unset>
2021-07-27 16:23:11 +0500

    added response and connection timeout to webclient

--- classroom-service/src/main/java/com/example/classservice/ClassroomServiceApplication.java
+++ classroom-service/src/main/java/com/example/classservice/ClassroomServiceApplication.java
@@ -1,28 +1,40 @@
 package com.example.classservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.channel.ChannelOption;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ClassroomServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClassroomServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomServiceApplication.class, args);
+    }
 
-	@Bean
-	@LoadBalanced
-	public WebClient.Builder getWebClientBuilder() {
-		return WebClient.builder();
-	}
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder getWebClientBuilder() {
 
-	public ObjectMapper getObjectMapper() {
-		return new ObjectMapper();
-	}
+        HttpClient client = HttpClient.create()
+				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+				.responseTimeout(Duration.ofSeconds(10));
+
+
+
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(client));
+    }
+
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
 }
